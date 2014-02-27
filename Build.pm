bddd03a508b19b7980abb07ec9112f7c927bcbf6
Tobias Leich <email@froggs.de>
2014-02-27 21:44:44 +0100

    the arg passed to build is not the project itself
    It is a string representing the current working diretory as of now,
    and it will be the same dir as an IO::Path in future.
    
    I decided to not specify any parameters because these are not used
    anyway.

--- Build.pm
+++ Build.pm
@@ -7,9 +7,7 @@ my $name = "IUP";
 my $libs = "-Wl,--no-as-needed -liup -liupimglib";
 
 class Build is Panda::Builder {
-    method build(Pies::Project $p) {
-        my $workdir = $.resources.workdir($p);
-
+    method build(|) {
 		my $c_line = "$*VM<config><cc> -c $*VM<config><cc_shared> $*VM<config><cc_o_out>src/$name$o "
 						~ "$*VM<config><ccflags> src/$name.c";
 		my $l_line = "$*VM<config><ld> $*VM<config><ld_load_flags> $*VM<config><ldflags> "
